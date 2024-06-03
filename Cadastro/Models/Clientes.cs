aa16879d20fc5426a179811bf7b13b1801255eb7
MateoRodrigues <matheus.machado.curso@gmail.com>
2024-06-03 14:27:15 -0300

    tirando pasta vscode

---
+++ Cadastro/Models/Clientes.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cadastro.Models
+{
+    public class Clientes
+    {
+        protected int Id;
+        protected string Nome;
+        protected int Idade;
+        public int Ativo{ get; set; }
+
+
+    }
+}
